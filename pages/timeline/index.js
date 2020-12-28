6e4127f56221b35e32412e1ab7f888e11c350dab
adria-arquimbau <adria.arquimbau@gmail.com>
2020-12-28 17:19:37 +0100

    feat: Timeline

---
+++ pages/timeline/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function Timeline(){
+    return (
+        <>
+            <h1>This is the timeline</h1>
+            <Link href='/'>Go Home</Link>
+            <style jsx>{`
+              h1 {
+              
+              }
+            `}</style>
+        </>
+    )
+}
