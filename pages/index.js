8293548fc578c52cb637709e309b29a3e6b242b0
adria-arquimbau <adria.arquimbau@gmail.com>
2020-12-29 01:30:19 +0100

    feat: user auth

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,61 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Button from "../components/Button";
+import {loginWithGithub, onAuthStateChange} from "../firebase/client";
+import {useState, useEffect} from 'react';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Template Next App 🔥</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Template Next.js!</a>
-        </h1>
-        <nav>
-          <Link href="/timeline">Timeline</Link>
-        </nav>
-       </main>
+    const [user, setUser] = useState(undefined)
 
-    </div>
-  )
+    useEffect(() => {
+        onAuthStateChange(user => setUser(user))
+    }, [])
+
+    const handleClick = () => {
+        loginWithGithub()
+            .then(setUser)
+            .catch(err => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <>
+            <div className={styles.container}>
+                <Head>
+                    <title>Template Next App 🔥</title>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+
+                <main className={styles.main}>
+                    <h1 className={styles.title}>
+                        Welcome to <a href="https://nextjs.org">Template Next.js!</a>
+                    </h1>
+                    <div>
+                        {
+                            user === null &&
+                            <Button onClick={handleClick}>
+                                Login with GitHub
+                            </Button>
+                        }
+                        {
+                            user && user.avatar && <div>
+                                <img src={user.avatar}/>
+                                <strong>{user.username}</strong>
+                            </div>
+                        }
+
+
+                    </div>
+                </main>
+            </div>
+
+            <style jsx>{`
+              div {
+                margin-top: 15px;
+              }
+            `}</style>
+        </>
+    )
 }
