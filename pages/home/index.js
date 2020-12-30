a163fd5d41030dbcc842d646e9b1055ee3894c57
adria-arquimbau <adria.arquimbau@gmail.com>
2020-12-30 13:00:24 +0100

    feat: firebase database

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,15 +1,17 @@
 import AppLayout from "../../components/AppLayout";
 import {useEffect, useState} from 'react'
 import Devit from "../../components/Devit";
+import useUser from "../../hooks/useUser";
 
 export default function HomePage(){
-    const [timeline, setTimeline] = useState([])
 
+    const [timeline, setTimeline] = useState([])
+    const user = useUser()
     useEffect(() => {
-        fetch("http://localhost:3000/api/statuses/home_timeline")
+        user && fetch("http://localhost:3000/api/statuses/home_timeline")
             .then((res) => res.json())
             .then(setTimeline)
-    }, [])
+    }, [user])
 
     return(
         <>
@@ -36,30 +38,32 @@ export default function HomePage(){
             </AppLayout>
 
             <style jsx>{`
-            header {
-              align-items: center;
-              border-bottom: 1px solid #ccc;
-              height: 49px;
-              display: flex;
-              position: sticky;
-              top: 0;
-              width: 100%;
-            }
-            h2 {
-              font-size: 21px;
-              font-weight: 800;
-            }
-            section {
-              padding-top: 49px;
-            }
-            nav {
-              bottom: 0;
-              border-top: 1px solid #ccc;
-              height: 49px;
-              position: sticky;
-              width: 100%;
-            }
-      `}</style>
+                header {
+                  align-items: center;
+                  background: #ffffffaa;
+                  backdrop-filter: blur(5px);
+                  border-bottom: 1px solid #eee;
+                  height: 49px;
+                  display: flex;
+                  position: sticky;
+                  top: 0;
+                  width: 100%;
+                }
+                h2 {
+                  font-size: 21px;
+                  font-weight: 800;
+                  padding-left: 15px;
+                }
+                nav {
+                  background: #fff;
+                  bottom: 0;
+                  border-top: 1px solid #eee;
+                  height: 49px;
+                  position: sticky;
+                  width: 100%;
+                }
+              `}
+            </style>
     </>
     )
 }
