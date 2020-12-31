61a5ec981ab1ec550acdaa469fb7542e5da9f14c
adria-arquimbau <adria.arquimbau@gmail.com>
2020-12-31 19:55:21 +0100

    feat: do a message

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
 
-                <main className={styles.main}>
+                  <main className={styles.main}>
                     <h1 className={styles.title}>
                         Welcome to <a href="https://nextjs.org">Template Next.js!</a>
                     </h1>
