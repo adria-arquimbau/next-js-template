dcef02dcfca42c8a67a2ec463b6c418479934f47
adria-arquimbau <adria.arquimbau@gmail.com>
2021-01-01 19:24:24 +0100

    feat: getStaticProps

--- hooks/useDateTimeFormat.js
+++ hooks/useDateTimeFormat.js
@@ -1,16 +1,16 @@
-// import { DEFAULT_LANGUAGE } from 'constants/locale'
-//error en el import
-export default function useDateTimeFormat (timestamp) {
+//import { DEFAULT_LANGUAGE } from "constants/locale"
+
+export default function useDateTimeFormat(timestamp) {
   const date = new Date(timestamp)
   const language = DEFAULT_LANGUAGE
 
   const options = {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
   }
 
   return new Intl.DateTimeFormat(language, options).format(date)
