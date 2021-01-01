6dd1e2314c156615960f46c5b336f38e8fcf7e99
adria-arquimbau <adria.arquimbau@gmail.com>
2021-01-01 19:24:17 +0100

    feat: getStaticProps

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,22 +1,35 @@
 import Avatar from '../avatar'
 import useTimeAgo from '../../hooks/useTimeAgo'
-import useDateTimeFormat from '../../hooks/useDateTimeFormat'
+import Link from 'next/link'
+import { useRouter } from "next/router"
+//import useDateTimeFormat from '../../hooks/useDateTimeFormat'
 
 export default function Devit ({ avatar, img, userName, content, createdAt, id }) {
-  const timeAgo = useTimeAgo(createdAt)
-  // const createdAtFormated = useDateTimeFormat(createdAt)
+    const timeago = useTimeAgo(createdAt)
+   // const createdAtFormated = useDateTimeFormat(createdAt)
 
-  return (
+    const router = useRouter()
+
+    const handleArticleClick = (e) => {
+        e.preventDefault()
+        router.push(`/status/${id}`)
+    }
+
+    return (
         <>
-            <article>
+            <article onClick={handleArticleClick}>
                 <div>
                     <Avatar alt={userName} src={avatar} />
                 </div>
                 <section>
                     <header>
                         <strong>{userName}</strong>
-                        <span> . </span>
-                        <time>{timeAgo}</time>
+                        <span> · </span>
+                        <Link href={`/status/${id}`}>
+                            <a>
+                                <time>{timeago}</time>
+                            </a>
+                        </Link>
                     </header>
                     <p>{content}</p>
                     {img && <img src={img} />}
@@ -27,6 +40,10 @@ export default function Devit ({ avatar, img, userName, content, createdAt, id }
           border-bottom: 1px solid #eee;
           display: flex;
           padding: 10px 15px;
+        }
+        article:hover {
+          background: #f5f8fa;
+          cursor: pointer;
         }
         img {
           border-radius: 10px;
@@ -41,11 +58,15 @@ export default function Devit ({ avatar, img, userName, content, createdAt, id }
           line-height: 1.3125;
           margin: 0;
         }
-        date {
+        a {
           color: #555;
           font-size: 14px;
+          text-decoration: none;
+        }
+        a:hover {
+          text-decoration: underline;
         }
       `}</style>
         </>
-  )
+    )
 }
