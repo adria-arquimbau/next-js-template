406d1c1051bcf42e49654ab0649f2993b6f2ae44
adria-arquimbau <adria.arquimbau@gmail.com>
2021-01-01 14:15:44 +0100

    feat: firebaseAdmin

--- components/Icons/Create.js
+++ components/Icons/Create.js
@@ -1,7 +1,7 @@
-import * as React from "react"
+import * as React from 'react'
 
-export default function Create(props) {
-    return (
+export default function Create (props) {
+  return (
         <svg height={21} viewBox="0 0 21 21" width={21} {...props}>
             <g
                 fill="none"
@@ -13,5 +13,5 @@ export default function Create(props) {
                 <path d="M17.5 3.467a1.462 1.462 0 01-.017 2.05L10.5 12.5l-3 1 1-3 6.987-7.046a1.409 1.409 0 012.013.013zM15.5 5.5l.953 1" />
             </g>
         </svg>
-    )
+  )
 }
