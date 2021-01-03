6ea6b971858f4f28e38c7f18da9125827bc66add
adria-arquimbau <adria.arquimbau@gmail.com>
2021-01-03 18:20:38 +0100

    feat: .env configs

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,42 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
+import { formatDate } from "./useDateTimeFormat"
+
+const isRelativeTimeFormatSupported =
+    typeof Intl !== "undefined" && Intl.RelativeTimeFormat
 
 const DATE_UNITS = [
-  ['year', 2419200 * 12],
-  ['month', 604800 * 4],
-  ['week', 86400 * 7],
-  ['day', 3600 * 24],
-  ['hour', 3600],
-  ['minute', 60],
-  ['second', 1]
+  ["day", 86400],
+  ["hour", 3600],
+  ["minute", 60],
+  ["second", 1],
 ]
 
-const getDateDiffs = timestamp => {
+const getDateDiffs = (timestamp) => {
   const now = Date.now()
   const elapsed = (timestamp - now) / 1000
 
   for (const [unit, secondsInUnit] of DATE_UNITS) {
-    if (Math.abs(elapsed) > secondsInUnit || unit === 'second') {
+    if (Math.abs(elapsed) > secondsInUnit || unit === "second") {
       const value = Math.round(elapsed / secondsInUnit)
       return { value, unit }
     }
   }
 }
 
-export default function useTimeAgo (timeStamp) {
-  const [timeAgo, setTimeAgo] = useState(() => getDateDiffs(timeStamp))
+export default function useTimeAgo(timestamp) {
+  const [timeago, setTimeago] = useState(() => getDateDiffs(timestamp))
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const newTimeAgo = getDateDiffs(timeStamp)
-      setTimeAgo(newTimeAgo)
-    }, 1000)
+    if (isRelativeTimeFormatSupported) {
+      const interval = setInterval(() => {
+        const newTimeAgo = getDateDiffs(timestamp)
+        setTimeago(newTimeAgo)
+      }, 5000)
 
-    return () => clearInterval(interval)
-  }, [timeStamp])
-  const rtf = new Intl.RelativeTimeFormat('en', {
-    style: 'short'
-  })
-  const { value, unit } = timeAgo
+      return () => clearInterval(interval)
+    }
+  }, [timestamp])
+
+  if (!isRelativeTimeFormatSupported) {
+    return formatDate(timestamp)
+  }
+
+  const rtf = new Intl.RelativeTimeFormat("es", { style: "short" })
+
+  const { value, unit } = timeago
 
   return rtf.format(value, unit)
 }
